ed25088807f3ed782f80f504e156bda9b3fb45ec
MarioBR04 <mario.barrera.rodriguez@gmail.com>
2025-05-23 00:34:54 -0700

    Firebase connected and tested

---
+++ README.md
@@ -0,0 +1,1 @@
+Proyecto de Cómputo Móvil / Modelos de Calidad
---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/widgets.dart';
+import 'firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
+  
+}
